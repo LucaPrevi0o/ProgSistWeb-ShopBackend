e60b08e195ebce0cf01640caae6a4ad4b85d9490
LucaPrevi0o <luca01.previati@edu.unife.it>
2026-03-10 09:50:28 +0100

    added back user ID in returned JSON

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,15 +1,13 @@
 class UserController < ApplicationController
   def index
     users = User.includes(:user_info).all
-
-    # render users with their associated user_info, excluding the user_id field
-    render json: users.as_json(include: { user_info: { except: [:id, :user_id] } }, except: [:id])
+    render json: users.as_json(include: { user_info: { except: [:id, :user_id] } })
   end
 
   def show
     user = User.includes(:user_info).find_by(id: params[:id])
     if user
-      render json: user.as_json(include: { user_info: { except: [:id, :user_id] } }, except: [:id])
+      render json: user.as_json(include: { user_info: { except: [:id, :user_id] } })
     else
       render json: { error: "User not found" }, status: :not_found
     end
