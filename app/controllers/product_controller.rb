ffe1116b1ed0de93e0b16bbe734cbc4e03ec5ac7
LucaPrevi0o <luca01.previati@edu.unife.it>
2026-03-10 09:51:08 +0100

    added product pagination for product list display

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -3,7 +3,10 @@ class ProductController < ApplicationController
   skip_before_action :authenticate_request!, only: [:index, :show]
 
   def index
-    products = Product.all
+    page = params[:page].to_i
+    page = 1 if page < 1
+    per_page = 8
+    products = Product.limit(per_page).offset((page - 1) * per_page)
     render json: products
   end
 
