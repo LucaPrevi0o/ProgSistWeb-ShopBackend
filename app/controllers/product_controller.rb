e16cf35af79fd349c4490892baf55d3d47b280ad
LucaPrevi0o <luca01.previati@edu.unife.it>
2026-04-10 08:43:47 +0200

    updated available categories from items in stock

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -12,11 +12,22 @@ class ProductController < ApplicationController
 
     products = filtered.limit(per_page).offset((page - 1) * per_page)
 
+    # expose total pages via response header so frontend can render pagination
+    total = filtered.count
+    total_pages = (total.to_f / per_page).ceil
+    response.headers['X-Total-Pages'] = total_pages.to_s
+
     render json: products
   end
 
   def show
     product = Product.find(params[:id])
     render json: product
+  end
+
+  def categories
+    # return only categories that have at least one product available (stock > 0)
+    cats = Product.where('stock > 0').distinct.order(:category).pluck(:category)
+    render json: cats
   end
 end
