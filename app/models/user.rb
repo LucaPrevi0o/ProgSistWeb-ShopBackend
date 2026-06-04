8a0bb1eafd15c2b00e66078020de635632b51fc9
Luca Previati <luca@previati.com>
2026-06-04 18:36:50 +0200

    Add user role helpers and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,19 @@
 class User < ApplicationRecord
+  ROLES = %w[USER ADMIN].freeze
+
   has_secure_password
 
   has_one :user_info, dependent: :destroy
   has_one :cart, dependent: :destroy
   has_many :payment_methods, through: :user_info
+
+  validates :role, inclusion: { in: ROLES }
+
+  def admin?
+    role == 'ADMIN'
+  end
+
+  def user?
+    role == 'USER'
+  end
 end
