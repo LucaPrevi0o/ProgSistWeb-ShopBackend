4780de5d7a8dbdaf6a331bea5f2501e2f776af3c
Luca Previati <luca@previati.com>
2026-06-11 09:12:40 +0200

    Restore full application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,10 +10,8 @@ class ApplicationController < ActionController::API
     return render_unauthorized unless header.present?
 
     token = header.to_s.split(' ').last
-
-    begin
-      secret = Rails.application.credentials.secret_key_base || Rails.application.secret_key_base
-      decoded = JWT.decode(token, secret, true, { algorithm: 'HS256' })
-      payload = decoded[0]
-      @current_user = User.find_by(id: payload['user_id'])
-      return render_unauthorized unless @current_user
+    secret = Rails.application.credentials.secret_key_base || Rails.application.secret_key_base
+    decoded = JWT.decode(token, secret, true, { algorithm: 'HS256' })
+    payload = decoded[0]
+    @current_user = User.find_by(id: payload['user_id'])
+    render_unauthorized unless @current_user
